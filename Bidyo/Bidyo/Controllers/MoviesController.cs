f4e0db20844611988a041af7e8348e2de034777d
kennethjayramos <kenneth.jay.ramos@gmail.com>
2020-03-24 21:16:47 +0800

    Models,Controllers,Views, Partial View

---
+++ Bidyo/Bidyo/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bidyo.Models;
+
+namespace Bidyo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        //Create a list of movies
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1 , Name = "Parasite"},
+                new Movie { Id = 2, Name = "Joker"},
+                new Movie { Id = 3, Name = "1917"}
+            };
+        }
+    }
+}
