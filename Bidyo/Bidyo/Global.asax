a756c5f08bdbe206ff9c3e1673f1ef79a2f25890
kennethjayramos <kenneth.jay.ramos@gmail.com>
2020-03-24 14:42:08 +0800

    000.Intial Project Setup

---
+++ Bidyo/Bidyo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bidyo.MvcApplication" Language="C#" %>
---
+++ Bidyo/Bidyo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bidyo.Startup))]
+namespace Bidyo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
